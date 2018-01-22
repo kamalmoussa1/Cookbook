93ecd1b4ec90c71df00bcf53555fa9541705b6cf
Kamal Moussa <kamalmoussa03@gmail.com>
2018-01-22 23:55:19 +0200

    fix and add new method

--- interview-prep/reverse-alphastr.cpp
+++ interview-prep/reverse-alphastr.cpp
@@ -24,27 +24,58 @@ std::string reverse_str(std::string str)
 
 bool check(char c)
 {
-  return (c >= 'a' && c<= 'z') || (c>='A' && c<='Z') || (c == ' ')? true : false;
+  return (c >= 'a' && c<= 'z') || (c>='A' && c<='Z')? true : false;
 }
 
-std::string reverse_alpha(std::string str) // O(n^2)
+std::string reverse_alpha(std::string str) // O(n)
 {
   std::string r_str;
-  std::string word; 
   for(int i =0; i < str.length() ; i++)
   {
     if(check(str[i]))
     {
-      word += str[i];
-    }
-    else 
-    {   
-      r_str += reverse_str(word) + str[i];
-      word = "";
+      r_str += str[i];
     }
   }
-  r_str += reverse_str(word);
-  return r_str;
+
+  r_str = reverse_str(r_str);
+
+  int j =0;
+  for(int i =0; i < str.length() ; i++)
+  {
+    if(check(str[i]))
+    {
+      str[i] = r_str[j];
+      j++;
+    }
+  }
+  return str;
+}
+
+// better- traverse n/2
+std::string reverse_alpha_2(std::string  str)
+{
+  int i = 0;
+  int j = str.length() -1;
+
+  while(i<j)
+  {
+    if(!check(str[i]))
+      i++;
+    else if(!check(str[j]))
+      j--;
+    else
+    {
+      //std::swap(str[i], str[j]);
+      // OR
+      char t = str[i];
+      str[i] = str[j];
+      str[j] = t;
+      i++;
+      j--;
+    }
+  }
+  return str;
 }
 
 using namespace std;
@@ -56,8 +87,12 @@ int main()
   string s3 = "Hello World";
 
 
-  cout << reverse_alpha(s1) << endl;
-  cout << reverse_alpha(s2) << endl;
-  cout << reverse_alpha(s3) << endl;
+  cout << reverse_alpha_2(s1) << endl;
+  cout << reverse_alpha(s1)   << endl;
+  cout << reverse_alpha_2(s2) << endl;
+  cout << reverse_alpha(s2)   << endl;
+  cout << reverse_alpha_2(s3) << endl;
+  cout << reverse_alpha(s3)   << endl;
+
 }
 
